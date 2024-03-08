20eeafdf1781569176ab662ea89af946b7f4d508
Thiago Freitas <thiagosistemas3@gmail.com>
2024-03-08 18:16:32 -0300

    Add Jenkinsfile and update route.tsx

--- app/routes/_app.instances/route.tsx
+++ app/routes/_app.instances/route.tsx
@@ -58,8 +58,6 @@ export default function AppIndex() {
   const sortOrder = searchParams.get("sort_order") || "";
   const page = searchParams.get("page") || "0";
   const pageSize = searchParams.get("page_size") || "10";
-  // const onlyActive = searchParams.get("only_active") || "true";
-  // const onlyStatusMonitor = searchParams.get("only_status_monitor") || "true";
 
   const [stateData, setState] = useState<InstancesModel | undefined>();
   const [fullColumns, setFullColumns] = useState(false);
